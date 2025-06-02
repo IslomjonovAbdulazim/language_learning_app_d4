ee8239dd8cf8756eff97f4288caba4d271c46b76
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-06-02 17:41:49 +0500

    imp

--- lib/models/auth_models.dart
+++ lib/models/auth_models.dart
@@ -1,1 +1,23 @@
-class LoginModel {}
+class LoginModel {
+  // members
+  late String email;
+  late String password;
+
+  // constructor
+  LoginModel({
+    required this.email,
+    required this.password,
+  });
+
+  // fromJson
+  LoginModel.fromJson(Map json) {
+    email = json["email"];
+    password = json["password"];
+  }
+
+  // toJson
+  Map toJson() => {
+    "email": email,
+    "password": password,
+  };
+}
