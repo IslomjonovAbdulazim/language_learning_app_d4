81c21bbb077d75458527df02f9384231872b38a1
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-06-02 18:34:26 +0500

    imp

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:language_learning_app_d4/widgets/button_widget.dart';
+import 'package:language_learning_app_d4/widgets/textfield_widget.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,6 +12,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +23,42 @@ class _LoginPageState extends State<LoginPage> {
           padding: const EdgeInsets.all(20),
           child: Center(
             child: Column(
-              children: [],
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFieldWidget(
+                  hint: "Email",
+                  focus: FocusNode(),
+                  controller: emailController,
+                ),
+                SizedBox(height: 10),
+                TextFieldWidget(
+                  hint: "Password",
+                  focus: FocusNode(),
+                  controller: emailController,
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Spacer(),
+                    CupertinoButton(
+                      padding: EdgeInsets.zero,
+                      onPressed: () {},
+                      child: Text(
+                        "Forgot Password",
+                        style: GoogleFonts.poppins(
+                          color: Color(0xff7C8BA0),
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                ButtonWidget(
+                  text: "Log In",
+                  onTap: () {},
+                ),
+              ],
             ),
           ),
         ),
