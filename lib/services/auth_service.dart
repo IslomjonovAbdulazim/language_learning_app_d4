8b59bbcffa9ffdff762a3e72cf3f8a264ac4bff3
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-06-04 18:22:02 +0500

    imp

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,1 +1,26 @@
-class AuthService {}
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthService {
+  static bool hasToken = false;
+  static String token = "";
+
+  static Future<void> init() async {
+    final db = await SharedPreferences.getInstance();
+    token = db.getString("token") ?? "";
+    hasToken = token.isNotEmpty;
+  }
+
+  static Future<void> login(String value) async {
+    final db = await SharedPreferences.getInstance();
+    await db.setString("token", value);
+    token = value;
+    hasToken = true;
+  }
+
+  static Future<void> logout() async {
+    final db = await SharedPreferences.getInstance();
+    await db.remove("token");
+    token = "";
+    hasToken = false;
+  }
+}
