b5b80be69a261b0b27db0ee43108ebd3e93b69e8
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-06-11 18:37:15 +0500

    imp

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:language_learning_app_d4/models/profile_models.dart';
 import 'package:language_learning_app_d4/providers/profile_provider.dart';
 
@@ -33,14 +35,37 @@ class _ProfilePageState extends State<ProfilePage> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
+        actions: [
+          CupertinoButton(
+            onPressed: () {
+              load();
+            },
+            child: Icon(
+              CupertinoIcons.refresh,
+              color: Color(0xff3461FD),
+              size: 28,
+            ),
+          ),
+        ],
       ),
       body: isLoading
           ? Center(
               child: CircularProgressIndicator(color: Color(0xff3461FD)),
             )
-          : ListView(
-              children: [],
-            ),
+          : profile == null
+              ? Center(
+                  child: Text(
+                    "Error",
+                    style: GoogleFonts.poppins(
+                      color: Colors.red,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 25,
+                    ),
+                  ),
+                )
+              : ListView(
+                  children: [],
+                ),
     );
   }
 }
