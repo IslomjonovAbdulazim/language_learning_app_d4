4c45513386330961b1b8a91f1321ada7e6c7e494
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-06-13 17:25:55 +0500

    imp

--- lib/providers/profile_provider.dart
+++ lib/providers/profile_provider.dart
@@ -51,4 +51,71 @@ class ProfileProvider {
     );
     return false;
   }
+
+  static Future<bool> updateBio(String bio) async {
+    final uri = Uri.parse(ApiConstants.profile);
+    final response = await http.put(
+      uri,
+      headers: {
+        "Authorization": "Bearer ${AuthService.token}",
+        "Content-Type": "application/json",
+      },
+      body: jsonEncode({"bio": bio}),
+    );
+    final body = jsonDecode(response.body);
+    if (response.statusCode == 200 && body["status_code"] == 200) {
+      Get.snackbar(
+        "Bio Changed Successfully",
+        "",
+        colorText: Colors.green.shade700,
+      );
+      return true;
+    }
+    Get.snackbar(
+      "Something went wrong",
+      "",
+      colorText: Colors.red.shade700,
+    );
+    return false;
+  }
+
+  static Future<bool> updateUsername(String username) async {
+    final uri = Uri.parse(ApiConstants.profile);
+    final response = await http.put(
+      uri,
+      headers: {
+        "Authorization": "Bearer ${AuthService.token}",
+        "Content-Type": "application/json",
+      },
+      body: jsonEncode({"username": username}),
+    );
+    final body = jsonDecode(response.body);
+    if (response.statusCode == 200 && body["status_code"] == 200) {
+      Get.snackbar(
+        "Username Changed Successfully",
+        "",
+        colorText: Colors.green.shade700,
+      );
+      return true;
+    }
+    Get.snackbar(
+      "Something went wrong",
+      "",
+      colorText: Colors.red.shade700,
+    );
+    return false;
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
