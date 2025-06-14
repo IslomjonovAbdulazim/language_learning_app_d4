0d0e16d95fa6f4eac143e846316d0ef7ed882d74
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-06-14 19:35:22 +0500

    imp

--- lib/pages/profile/profile_name_page.dart
+++ lib/pages/profile/profile_name_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:language_learning_app_d4/providers/profile_provider.dart';
+import 'package:language_learning_app_d4/widgets/button_widget.dart';
+import 'package:language_learning_app_d4/widgets/textfield_widget.dart';
 
 class ProfileNamePage extends StatefulWidget {
   final String name;
@@ -10,10 +15,71 @@ class ProfileNamePage extends StatefulWidget {
 }
 
 class _ProfileNamePageState extends State<ProfileNamePage> {
+  final nameController = TextEditingController();
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    nameController.text = widget.name;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text(
+          "Name",
+          style: GoogleFonts.poppins(),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsetsGeometry.all(20),
+          child: Column(
+            children: [
+              TextFieldWidget(
+                hint: "Name",
+                focus: FocusNode(),
+                controller: nameController,
+              ),
+              SizedBox(height: 20),
+              isLoading
+                  ? CircularProgressIndicator(color: Color(0xff3461FD))
+                  : ButtonWidget(
+                      onTap: () async {
+                        final text = nameController.text.trim();
+                        if (text.length < 3 || text.length > 15) {
+                          Get.snackbar("Name is too short or long", "");
+                        } else if (text == widget.name) {
+                          Get.back();
+                        } else {
+                          isLoading = true;
+                          setState(() {});
+                          final res = await ProfileProvider.updateName(text);
+                          if (res) {
+                            Get.back();
+                          } else {
+                            isLoading = false;
+                            setState(() {});
+                          }
+                        }
+                      },
+                      text: "Save",
+                    ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
