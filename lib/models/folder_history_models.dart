46791946763270096597fd2c621f2a58f72ddfb9
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-06-27 17:23:19 +0500

    imp

--- lib/models/folder_history_models.dart
+++ lib/models/folder_history_models.dart
@@ -3,6 +3,15 @@ class FolderHistoryModel {
   late String folderOwner;
   late bool isMyOwnFolder;
   late List<QuizHistoryModel> quizHistory;
+
+  FolderHistoryModel.fromJson(Map json) {
+    final data = json["data"];
+    folderTitle = data["folder_title"];
+    folderOwner = data["folder_owner"];
+    isMyOwnFolder = data["is_my_own_folder"];
+    final json = List.from(data["quiz_history"]);
+    quizHistory = json.map((j) => QuizHistoryModel.fromJson(i)).toList();
+  }
 }
 
 class QuizHistoryModel {
@@ -11,4 +20,12 @@ class QuizHistoryModel {
   late int correctAnswers;
   late int totalQuestions;
   late DateTime completedAt;
+
+  QuizHistoryModel.fromJson(Map json) {
+    quizId = json["quiz_id"];
+    score = json["score"];
+    correctAnswers = json["correct_answers"];
+    totalQuestions = json["total_questions"];
+    completedAt = DateTime.parse(json["completed_at"]);
+  }
 }
